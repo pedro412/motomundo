f9062a7b5e944a96d221d642114a7f97d15a548d
pedro412 <pedro.alvarez412@outlook.com>
2025-08-27 14:31:37 -0600

    increase page size for chapters

--- motomundo/settings.py
+++ motomundo/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 50,
 }
 
 # JWT Settings
